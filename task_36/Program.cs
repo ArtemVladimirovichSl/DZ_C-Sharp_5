824f3e57bf9becb2674b739e91b3ebf42039966a
Artem <slivnitsin.art@mail.ru>
2022-09-03 02:04:41 +0800

    task_36

---
+++ task_36/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[4];
+int Sum = 0;
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+    if(i%2 != 0)
+    {
+        Sum += array[i];
+        count++;
+    }
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.Write("Сумма элементов, стоящих на нечётных позициях = " + Sum);
