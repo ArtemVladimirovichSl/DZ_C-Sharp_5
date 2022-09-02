23b17900c25f323c2d66073c296078c8f6ede1de
Artem <slivnitsin.art@mail.ru>
2022-09-03 02:05:18 +0800

    task_38

---
+++ task_38/Program.cs
@@ -0,0 +1,16 @@
+﻿double[] array = {3, 7, 22, 2, 78};
+double min = array[0]; 
+double max = array[0];
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(min > array[i])
+    {
+        min = array[i]; 
+    }
+    if(max < array[i])
+    {
+        max = array[i];
+    }
+}
+Console.Write("Разница между максимальным и минимальным элементами массива = " + (max - min));
