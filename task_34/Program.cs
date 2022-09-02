0bedfd0fa8aadde84302617328fb4fb5c433f018
Artem <slivnitsin.art@mail.ru>
2022-09-03 02:04:07 +0800

    task_34

---
+++ task_34/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[4]; 
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99,1000);
+    if(array[i]%2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("[{0}]", String.Join(", ", array));
+Console.Write("Чётных чисел в массиве = " + count);
